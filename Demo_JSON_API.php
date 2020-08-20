2b3a925ffd2cc584ad9ea08f37cde2222b43f9b9
Albdroid <sxt@usa.com>
2020-08-20 20:41:55 +0200

    Demo Data
    Code is for sale

---
+++ Demo_JSON_API.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set("Europe/Tirane");
+
+/*
+ Regex Generator @ Kodi dot AL Dev Tools
+ Code For PHP 5/7
+*/
+
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+$GET_URL = file_get_contents('https://paidcodes.albdroid.al/Vimeo_Apis/JSON_API.php'.$_SERVER['QUERY_STRING']);
+echo $GET_URL;
+?>
