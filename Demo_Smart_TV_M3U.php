7558dde8cb24b3cd6e12a9240eda0ecd5ed8d792
Albdroid.AL <sxt@usa.com>
2020-12-22 18:38:21 +0100

    Update Demo_Smart_TV_M3U.php

--- Demo_Smart_TV_M3U.php
+++ Demo_Smart_TV_M3U.php
@@ -12,6 +12,6 @@ date_default_timezone_set("Europe/Tirane");
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
-$GET_URL = file_get_contents('https://paidcodes.albdroid.al/Vimeo_Apis/Smart_TV_M3U.php'.$_SERVER['QUERY_STRING']);
+$GET_URL = file_get_contents('https://paidcodes.albdroid.al/Vimeo_APIS/Smart_TV_M3U.php'.$_SERVER['QUERY_STRING']);
 echo $GET_URL;
 ?>
